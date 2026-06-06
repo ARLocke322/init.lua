3da236cf37661b2e230202e240d15f069d4667ba
Alex Locke <alocke322@gmail.com>
2026-06-06 12:03:41 +0100

    add tokyo night theme, fix autoformat

--- plugin/colourschemes.lua
+++ plugin/colourschemes.lua
@@ -3,7 +3,14 @@ vim.pack.add({{
   name = 'rose-pine' 
 }})
 
-vim.cmd 'colorscheme rose-pine-moon'
+vim.pack.add({{
+  src = 'https://github.com/folke/tokyonight.nvim', 
+  name = 'tokyo-night' 
+}})
+
+-- vim.cmd 'colorscheme rose-pine-moon'
+vim.cmd 'colorscheme tokyonight-moon'
+
 vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#908caa' }) -- lines above cursor
 vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#908caa' }) -- lines below cursor
 vim.api.nvim_set_hl(0, 'LineNr', { fg = '#e0def4' }) -- current line number
